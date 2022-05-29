3eca7eb9654a73728e07da76ea9d0e938b1b42e9
Redice1997 <roshin-vladimir@mail.ru>
2022-05-29 18:21:37 +0300

    Определение класса команды закрытия приложения

--- QuipuGmbH/Infrastructure/Commands/Base/Command.cs
+++ QuipuGmbH/Infrastructure/Commands/Base/Command.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace QuipuGmbH.Infrastructure.Base
+namespace QuipuGmbH.Infrastructure.Commands.Base
 {
     public abstract class Command : ICommand
     {
