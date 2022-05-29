a766cf585efe0c6206d74957aa0412acb408fb4d
Redice1997 <roshin-vladimir@mail.ru>
2022-05-29 17:24:05 +0300

    создана базовая инфраструктура проекта, добавлены абстракции

---
+++ QuipuGmbH/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using QuipuGmbH.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuipuGmbH.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        #region Status : string - Статус программы
+        /// <summary>Статус программы</summary>
+        private string status = "Готов!";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get { return status; }
+            set { Set(ref status, value); }
+        }
+        #endregion
+    }
+}
