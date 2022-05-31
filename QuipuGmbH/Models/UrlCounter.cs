8be9556f886c83b9abb5e3bba3fffc2c46f04088
Redice1997 <roshin-vladimir@mail.ru>
2022-05-31 20:18:22 +0300

    Добавлена модель содержащая число тегов

---
+++ QuipuGmbH/Models/UrlCounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuipuGmbH.Models
+{
+    class UrlCounter
+    {
+        public string Url { get; set; }
+        public int TagCount { get; set; }
+    }
+}
