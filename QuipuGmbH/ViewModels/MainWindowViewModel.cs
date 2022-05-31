53aa90ed674c45ca91152457546e1b6e99974172
Redice1997 <roshin-vladimir@mail.ru>
2022-05-31 20:19:16 +0300

    Создано поле содержащее список моделей счетчиков

--- QuipuGmbH/ViewModels/MainWindowViewModel.cs
+++ QuipuGmbH/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
-﻿using QuipuGmbH.ViewModels.Base;
+﻿using QuipuGmbH.Models;
+using QuipuGmbH.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,5 +32,26 @@ namespace QuipuGmbH.ViewModels
             set => Set(ref status, value);
         }
         #endregion
+
+        #region UrlCounters : ObservableCollection<UrlCounter> - Список URL с количеством тегов в каждом
+
+        /// <summary>Список URL с количеством тегов в каждом</summary>
+        private ObservableCollection<UrlCounter> _UrlCounters;
+
+        /// <summary>Список URL с количеством тегов в каждом</summary>
+        public ObservableCollection<UrlCounter> UrlCounters { get => _UrlCounters; set => Set(ref _UrlCounters, value); }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            var urlCounters = Enumerable.Range(1, 20).Select(i => new UrlCounter
+            {
+                Url = $"https://url{i}",
+                TagCount = new Random().Next(500)
+            }).OrderByDescending(url => url.TagCount);
+
+            UrlCounters = new ObservableCollection<UrlCounter>(urlCounters);
+        }
     }
 }
